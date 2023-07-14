a76de0e981a7b516b6a7b8799cee7ef39668fb0b
mouseylicense <nevobloch@gmail.com>
2023-07-14 17:07:09 +0300

    first commit

--- static/script3.js
+++ static/script3.js
@@ -2,17 +2,20 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const bodyParser = require('body-parser');
-
 var loginbutton = document.getElementById('button');
-var closeBtn = document.querySelector('.close-btn');
-
+console.log(popup2)
 app.set('views', path.join(__dirname, "views"));
-
+popup2.innerHTML = "test"
 function checkCreds(){
+  var closeBtn = document.getElementById("closeBtn")
+  var closeBtn = document.getElementById("closeBtn")
+
+  var popup2 = document.getElementById('popup');
+
+
   // checks the values from the form inputs
   var name = document.getElementById('email').value;
   var password = document.getElementById('password').value;
-  var closeBtn = document.getElementById("closeBtn")
   fetch("/loginButton", {
     method: "POST",
     body: JSON.stringify({
@@ -26,10 +29,11 @@ function checkCreds(){
   .then((response) => response.json())
   .then((json) => {if (json["Exists"] == false)
   {
+    console.log(popup2.innerHTML)
     console.log(json["Exists"])
-    popup.style.display = 'block';
+    popup2.style.display = 'block';
     closeBtn.addEventListener('click', function () {
-      popup.style.display = 'none';
+      popup2.style.display = 'none';
   })} else if (json["Exists"] == "Admin") {
     window.location.href = '/indexAdmin';
 }else if (json["Exists"] == true){
